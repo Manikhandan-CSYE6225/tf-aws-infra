83dd7b273f0f7e06954117403740ed0582dac721
Manikhandan Mavureddy Senthilvelu <mavureddysenthilve.m@northeastern.edu>
2025-02-24 16:32:50 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "my_vpc" {
   count                = var.vpc_count
   cidr_block           = var.vpc_cidrs[count.index]
   enable_dns_support   = true
-      enable_dns_hostnames = true
+  enable_dns_hostnames = true
 
   tags = {
     Name = "${var.vpc_name}-${count.index}"
